66873ee26c60170cf0569dbe9cce2e10eea71b3d
Darren Kitching <39224893+DarrenKitching@users.noreply.github.com>
2019-11-10 20:15:24 +0000

    Set up forms redirect

--- Social Graph/SocialGraph/Graph/urls.py
+++ Social Graph/SocialGraph/Graph/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import MyBar, MyPie
+from .views import MyBar, MyPie, index, submit
 
 
 urlpatterns = [
+	path('', index, name='index'),
+	path('submit', submit, name='index'),
 	path('bar', MyBar.as_view(),name='myplots'),
 	path('pie', MyPie.as_view(),name='myplots'),
 ]
--- Social Graph/SocialGraph/Graph/views.py
+++ Social Graph/SocialGraph/Graph/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
+from django.template import Context, loader
+from django.http import HttpResponse, HttpResponseRedirect
 from . import plots
+
+name = None
+password = None
 
 class MyBar(TemplateView):
     template_name = 'plot.html'
@@ -18,3 +23,12 @@ class MyPie(TemplateView):
         password = ''
         context['myplots'] = plots.getPieChartData(name, password)
         return context
+
+def index(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+def submit(request):
+    name = request.POST.get('uname')
+    password = request.POST.get('psw')
+    return HttpResponseRedirect('bar')
